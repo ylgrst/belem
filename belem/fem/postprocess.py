7c2bafe025e7821921cd9708de93e793a93cb54c
Yasmin Legerstee <125015797+ylgrst@users.noreply.github.com>
2024-06-04 18:54:41 +0200

    Implement correct yield surface plotting

--- belem/fem/postprocess.py
+++ belem/fem/postprocess.py
@@ -35,6 +35,7 @@ def compute_von_mises_plastic_strain(dataset: fd.DataSet) -> npt.NDArray[np.floa
     return plastic_strain_array
 
 def diagonalize_stress_tensor(voigt_stress_tensor: npt.NDArray[np.float_]) -> npt.NDArray[np.float_]:
+    """Returns diagonalized stress matrix"""
     if np.shape(voigt_stress_tensor) != (6,):
         raise ValueError("Voigt stress tensor is not a 6x1 vector")
     stress_tensor = np.zeros((3,3))
@@ -45,54 +46,93 @@ def diagonalize_stress_tensor(voigt_stress_tensor: npt.NDArray[np.float_]) -> np
                 stress_tensor[i,j] = voigt_stress_tensor[i+j+2]
                 stress_tensor[j,i] = stress_tensor[i,j]
 
-    return stress_tensor
+    eigenvalues, _ = np.linalg.eig(stress_tensor)
 
-def compute_principal_stresses()
+    return eigenvalues
 
-def get_stress_strain_at_plasticity_threshold(stress_array: npt.NDArray[np.float_], plasticity_array: npt.NDArray[np.float_], plasticity_threshold: np.float_, max_strain: np.float_ = 0.05):
+def compute_principal_stresses(dataset: fd.DataSet) -> npt.NDArray[np.float_]:
+    """Returns principal stresses arrays"""
+    component_to_voigt: dict[str, int] = {"XX": 0, "YY": 1, "ZZ": 2, "XY": 3, "XZ": 4, "YZ":5}
+    component_list = ["XX", "YY", "ZZ", "XY", "XZ", "YZ"]
+    mesh_volume = dataset.mesh.to_pyvista().volume
+    rve_volume = dataset.mesh.bounding_box.volume
+    density = mesh_volume / rve_volume
+    n_iter = dataset.n_iter
+    principal_stresses_array = np.zeros((3, n_iter))
+    for i in range(n_iter):
+        dataset.load(i)
+        stress_array = np.zeros(6)
+        for component in component_list:
+            data_stress = dataset.get_data(field="Stress", component=component, data_type="GaussPoint")
+            vol_avg_stress = (density / mesh_volume) * dataset.mesh.integrate_field(field=data_stress,
+                                                                                    type_field="GaussPoint")
+            stress_array[component_to_voigt[component]] = vol_avg_stress
+        principal_stresses = diagonalize_stress_tensor(stress_array)
+        principal_stresses_array[:, i] = principal_stresses
+
+    return principal_stresses_array
+
+def create_plastic_strain_and_principal_stress_data(dataset: fd.DataSet) -> tuple[npt.NDArray[np.float_], npt.NDArray[np.float_]]:
+    p_stresses = compute_principal_stresses(dataset)
+    vm_plastic_strain = compute_von_mises_plastic_strain(dataset)
+
+    return vm_plastic_strain, p_stresses
+
+def get_stress_strain_at_plasticity_threshold(stress_array: npt.NDArray[np.float_], plasticity_array: npt.NDArray[np.float_], plasticity_threshold: float):
     """Fetches point in stress-strain curve where set plasticity threshold is reached"""
-    strain_array = 100 * np.linspace(0, max_strain, 101)
     index = next(idx for idx, plasticity in enumerate(plasticity_array) if plasticity > plasticity_threshold)
-    strain = strain_array[index]
-    stress = stress_array[index]
+    stress = stress_array[:,index]
 
-    return strain, stress
+    return stress
 
-def plot_stress_strain(stress_array: npt.NDArray[np.float_], strain_array: npt.NDArray[np.float_]):
+def plot_stress_strain(stress_array: npt.NDArray[np.float_], strain_array: npt.NDArray[np.float_], figname: str = "stress_strain.png"):
     plt.figure()
     plt.plot(strain_array, stress_array, 'o-')
     plt.title("Stress vs strain")
     plt.xlabel("Strain (%)")
     plt.ylabel("Stress (MPa)")
-    plt.savefig("stress_strain.png")
+    plt.savefig(figname)
     plt.close()
 
-def plot_hardening(stress_array: npt.NDArray[np.float_], plasticity_array: npt.NDArray[np.float_]):
+def plot_hardening(stress_array: npt.NDArray[np.float_], plasticity_array: npt.NDArray[np.float_], figname: str = "hardening.png"):
     plt.figure()
     plt.plot(plasticity_array, stress_array, 'o-')
     plt.title("Stress vs plastic strain")
     plt.xlabel("Plastic strain (%)")
     plt.ylabel("Stress (MPa)")
-    plt.savefig("hardening.png")
+    plt.savefig(figname)
     plt.close()
 
-def plot_2d_yield_surface(tension_data: fd.DataSet, biaxial_tension_data: fd.DataSet, shear_data: fd.DataSet, compression_data: fd.DataSet, biaxial_compression_data: fd.DataSet, plasticity_threshold: np.float_, figname: str = "stress_at_Ep.png"):
-    tension_strain, tension_stress, tension_plastic_strain = compute_stress_strain_arrays(tension_data, "XX", "XX")
-    compression_strain, compression_stress, compression_plastic_strain = compute_stress_strain_arrays(compression_data, "XX", "XX")
-    biaxial_tension_strain, biaxial_tension_stress, biaxial_tension_plastic_strain = compute_stress_strain_arrays(biaxial_tension_data, "XY", "XY")
-    biaxial_compression_strain, biaxial_compression_stress, biaxial_compression_plastic_strain = compute_stress_strain_arrays(biaxial_compression_data, "XY", "XY")
-    shear_strain, shear_stress, shear_plastic_strain = compute_stress_strain_arrays(shear_data, "XY", "XY")
+def plot_yield_surface(tension_data: tuple[npt.NDArray[np.float_], npt.NDArray[np.float_]],
+                          biaxial_tension_data: tuple[npt.NDArray[np.float_], npt.NDArray[np.float_]],
+                          shear_data: tuple[npt.NDArray[np.float_], npt.NDArray[np.float_]],
+                          compression_data: tuple[npt.NDArray[np.float_], npt.NDArray[np.float_]],
+                          biaxial_compression_data: tuple[npt.NDArray[np.float_], npt.NDArray[np.float_]],
+                          plasticity_threshold: float, figname: str = "stress_at_Ep.png"):
 
-    stress_at_plastic_strain_threshold_tension = get_stress_strain_at_plasticity_threshold(tension_stress, tension_plastic_strain, plasticity_threshold)
-    stress_at_plastic_strain_threshold_compression = get_stress_strain_at_plasticity_threshold(compression_stress, compression_plastic_strain, plasticity_threshold)
-    stress_at_plastic_strain_threshold_biaxial_tension = get_stress_strain_at_plasticity_threshold(biaxial_tension_stress, biaxial_tension_plastic_strain, plasticity_threshold)
-    stress_at_plastic_strain_threshold_biaxial_compression = get_stress_strain_at_plasticity_threshold(biaxial_compression_stress, biaxial_compression_plastic_strain, plasticity_threshold)
-    stress_at_plastic_strain_threshold_shear = get_stress_strain_at_plasticity_threshold(shear_stress, shear_plastic_strain, plasticity_threshold)
 
-    plt.figure()
-    plot_data = [stress_at_plastic_strain_threshold_tension, stress_at_plastic_strain_threshold_biaxial_tension, stress_at_plastic_strain_threshold_tension, stress_at_plastic_strain_threshold_shear, -stress_at_plastic_strain_threshold_compression, -stress_at_plastic_strain_threshold_biaxial_compression, -stress_at_plastic_strain_threshold_compression, stress_at_plastic_strain_threshold_shear, stress_at_plastic_strain_threshold_tension]
-    theta = np.deg2rad(np.arange(0, 361, 45))
-    rcos = [plot_data[i] * np.cos(theta[i]) for i in range(len(theta))]
-    rsin = [plot_data[i] * np.sin(theta[i]) for i in range(len(theta))]
-    plt.plot(rcos, rsin, 'o--')
+    stress_at_plastic_strain_threshold_tension = get_stress_strain_at_plasticity_threshold(tension_data[1], tension_data[0], plasticity_threshold)
+    stress_at_plastic_strain_threshold_compression = get_stress_strain_at_plasticity_threshold(compression_data[1], compression_data[0], plasticity_threshold)
+    stress_at_plastic_strain_threshold_biaxial_tension = get_stress_strain_at_plasticity_threshold(biaxial_tension_data[1], biaxial_tension_data[0], plasticity_threshold)
+    stress_at_plastic_strain_threshold_biaxial_compression = get_stress_strain_at_plasticity_threshold(biaxial_compression_data[1], biaxial_compression_data[0], plasticity_threshold)
+    stress_at_plastic_strain_threshold_shear = get_stress_strain_at_plasticity_threshold(shear_data[1], shear_data[0], plasticity_threshold)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.axis('equal')
+    ax.spines['left'].set_position('zero')
+    ax.spines['bottom'].set_position('zero')
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    plt.xlabel(r"$\sigma_{11}$ (MPa)")
+    plt.ylabel(r"$\sigma_{22}$ (MPa)")
+    ax.xaxis.set_label_coords(1.05, 0.45)
+    ax.yaxis.set_label_coords(0.45, 1.05)
+
+    plot_data_x = [stress_at_plastic_strain_threshold_tension[0], stress_at_plastic_strain_threshold_biaxial_tension[0], 0.0, -stress_at_plastic_strain_threshold_shear[0], stress_at_plastic_strain_threshold_compression[0], stress_at_plastic_strain_threshold_biaxial_compression[0], 0.0, stress_at_plastic_strain_threshold_shear[0], stress_at_plastic_strain_threshold_tension[0]]
+    plot_data_y = [0.0, stress_at_plastic_strain_threshold_biaxial_tension[1], stress_at_plastic_strain_threshold_tension[0], -stress_at_plastic_strain_threshold_shear[1], 0.0, stress_at_plastic_strain_threshold_biaxial_compression[1], -stress_at_plastic_strain_threshold_tension[0], stress_at_plastic_strain_threshold_shear[1], 0.0]
+    plt.plot(plot_data_x, plot_data_y, "o--")
+
     plt.savefig(figname)
