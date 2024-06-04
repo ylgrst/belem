b9e97812035b632d7bb586bffb0fc6326e69dd9b
Yasmin Legerstee <125015797+ylgrst@users.noreply.github.com>
2024-06-04 16:11:04 +0200

    Implement von mises plastic strain computation

--- belem/fem/postprocess.py
+++ belem/fem/postprocess.py
@@ -2,26 +2,52 @@ import numpy as np
 import numpy.typing as npt
 import fedoo as fd
 import matplotlib.pyplot as plt
+from simcoon import simmit as sim
 
-def compute_stress_strain_plastic_arrays(dataset : fd.DataSet, stress_component: str, plastic_strain_component: str, max_strain: np.float_ = 0.05):
-    """Returns stress, strain and plastic strain arrays"""
-    component_to_voigt: dict[str, int] = {"XX": 0, "YY": 1, "ZZ": 2, "XY": 3, "XZ": 4, "YZ": 5}
-    volume = dataset.mesh.to_pyvista().volume
+def compute_average_stress_strain_arrays(dataset : fd.DataSet, component: str, max_strain: np.float_ = 0.05) -> dict[str, npt.NDArray[np.float_]]:
+    """Returns average stress and strain arrays"""
+    mesh_volume = dataset.mesh.to_pyvista().volume
+    rve_volume = dataset.mesh.bounding_box.volume
+    density = mesh_volume/rve_volume
     n_iter = dataset.n_iter
     stress_array = np.zeros(n_iter)
-    plastic_strain_array = np.zeros(n_iter)
     strain_array = 100 * np.linspace(0, max_strain, 101)
     for i in range(n_iter):
         dataset.load(i)
-        data_stress = dataset.get_data(field = "Stress", component = stress_component, data_type = "GaussPoint")
-        vol_avg_stress = dataset.mesh.integrate_field(field=data_stress, type_field="GaussPoint")/volume
+        data_stress = dataset.get_data(field = "Stress", component = component, data_type = "GaussPoint")
+        vol_avg_stress = (density/mesh_volume)*dataset.mesh.integrate_field(field=data_stress, type_field="GaussPoint")
         stress_array[i] = vol_avg_stress
-        data_Ep = dataset.get_data(field="Statev", data_type="GaussPoint")[2:8][
-            component_to_voigt[plastic_strain_component]]
-        vol_avg_Ep = dataset.mesh.integrate_field(field=data_Ep, type_field="GaussPoint") / volume
-        plastic_strain_array[i] = vol_avg_Ep
 
-    return strain_array, stress_array, 100*plastic_strain_array
+    return {"strain": strain_array, "stress": stress_array}
+
+def compute_von_mises_plastic_strain(dataset: fd.DataSet) -> npt.NDArray[np.float_]:
+    """Returns von mises plastic strain array"""
+    mesh_volume = dataset.mesh.to_pyvista().volume
+    n_iter = dataset.n_iter
+    plastic_strain_array = np.zeros(n_iter)
+    for i in range(n_iter):
+        dataset.load(i)
+        data_Ep = dataset.get_data(field="Statev", data_type="GaussPoint")[2:8]
+        data_vm_Ep = np.asarray([sim.Mises_strain(data_Ep[:,i]) for i in range(np.shape(data_Ep)[1])])
+        vol_avg_vm_Ep = dataset.mesh.integrate_field(field=data_vm_Ep, type_field="GaussPoint")/mesh_volume
+        plastic_strain_array[i] = 100*vol_avg_vm_Ep
+
+    return plastic_strain_array
+
+def diagonalize_stress_tensor(voigt_stress_tensor: npt.NDArray[np.float_]) -> npt.NDArray[np.float_]:
+    if np.shape(voigt_stress_tensor) != (6,):
+        raise ValueError("Voigt stress tensor is not a 6x1 vector")
+    stress_tensor = np.zeros((3,3))
+    for i in range(3):
+        stress_tensor[i,i] = voigt_stress_tensor[i]
+        for j in range(3):
+            if i<j:
+                stress_tensor[i,j] = voigt_stress_tensor[i+j+2]
+                stress_tensor[j,i] = stress_tensor[i,j]
+
+    return stress_tensor
+
+def compute_principal_stresses()
 
 def get_stress_strain_at_plasticity_threshold(stress_array: npt.NDArray[np.float_], plasticity_array: npt.NDArray[np.float_], plasticity_threshold: np.float_, max_strain: np.float_ = 0.05):
     """Fetches point in stress-strain curve where set plasticity threshold is reached"""
