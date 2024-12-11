fc01a960bdc9ef631e7fc0cc9df023a5f48959b5
ylgrst <125015797+ylgrst@users.noreply.github.com>
2024-12-11 18:27:03 +0100

    First implementation of dfa and identification subsections

--- belem/fem/report.py
+++ belem/fem/report.py
@@ -40,7 +40,8 @@ def generate_chapter_page(doc: pl.document.Document, shape: str):
     doc.append(pl.NewPage())
 
 def generate_section(doc: pl.document.Document, shape: str, density: float):
-    ...
+    generate_dfa_subsection(doc)
+    generate_identification_subsection(doc)
 
 def generate_response_curves_subsection(doc: pl.document.Document):
     ...
@@ -52,7 +53,37 @@ def generate_stress_concentration_map_subsection(doc: pl.document.Document):
     ...
 
 def generate_dfa_subsection(doc: pl.document.Document):
-    ...
+    with doc.create(pl.Subsection("Deshpande-Fleck-Ashby yield surface identification", numbering=False)):
+        with doc.create(pl.Figure(position="H")) as dfa_figs:
+            with doc.create(pl.SubFigure(position="t", width=NoEscape(r"0.45\textwidth"))) as dfa_1122_fig:
+                dfa_1122_fig.add_image(pl.NoEscape(r"example-image-duck"), width=pl.NoEscape(r"0.95\textwidth"))
+            with doc.create(pl.SubFigure(position="t", width=NoEscape(r"0.45\textwidth"))) as dfa_1112_fig:
+                dfa_1112_fig.add_image(pl.NoEscape(r"example-image-duck"), width=pl.NoEscape(r"0.95\textwidth"))
+            dfa_figs.add_caption("Simulated yield surface and identified Deshpande-Fleck-Ashby yield surface. Left: (S11,S22) plane. Right: (S11,S12) plane.")
+
+        with doc.create(pl.Table(position="H")) as dfa_tab:
+            with doc.create(pl.Tabular("|c|c|c|c|c|c|c|")) as dfa_table:
+                dfa_table.add_hline()
+                dfa_table.add_row(("F", "G", "H", "L", "M", "N", "K"))
+                dfa_table.add_hline()
+                dfa_table.add_row((0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0))
+                dfa_table.add_hline()
+            dfa_tab.add_caption("Identified Deshpande-Fleck-Ashby yield surface parameters")
+
 
 def generate_identification_subsection(doc: pl.document.Document):
-    ...
+    with doc.create(pl.Subsection("Homogenized behavior law identification", numbering=False)):
+        with doc.create(pl.MiniPage(width=r"0.5\textwidth")):
+            with doc.create(pl.Figure(position="H")) as ident_graph_pic:
+                ident_graph_pic.add_image(pl.NoEscape(r"example-image-duck"), width=pl.NoEscape(r"0.95\textwidth"))
+                ident_graph_pic.add_caption("Comparison between simulated behavior and identified non linear behavior law")
+
+        with doc.create(pl.MiniPage(width=r"0.5\textwidth")):
+            with doc.create(pl.Table(position="H")) as law_param_tab:
+                with doc.create(Tabular("|c|c|c|c|c|c|c|c|c|c|")) as law_param_table:
+                    law_param_table.add_hline()
+                    law_param_table.add_row(("E", pl.Math(data=pl.NoEscape(r"\nu"), inline=True), "G", pl.Math(data=pl.NoEscape(r"\alpha"), inline=True), "Q", "b", pl.Math(data=pl.NoEscape("C_{1}"), inline=True), pl.Math(data=pl.NoEscape("D_{1}"), inline=True), pl.Math(data=pl.NoEscape("C_{2}"), inline=True), pl.Math(data=pl.NoEscape("D_{2}"), inline=True)))
+                    law_param_table.add_hline()
+                    law_param_table.add_row((0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0))
+                    law_param_table.add_hline()
+                law_param_tab.add_caption("Homogenized law parameters")
