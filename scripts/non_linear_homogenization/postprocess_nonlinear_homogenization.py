853d06cc465d67d0f91056c5539ce83aa41e2bfb
ylgrst <125015797+ylgrst@users.noreply.github.com>
2025-02-12 13:35:53 +0100

    Print steps in non linear homogenizarion postprocess example

--- scripts/non_linear_homogenization/postprocess_nonlinear_homogenization.py
+++ scripts/non_linear_homogenization/postprocess_nonlinear_homogenization.py
@@ -2,24 +2,26 @@ from pathlib import Path
 import numpy as np
 import os
 from belem.fem.postprocess import *
+from simcoon import simmit as sim
 
 basedir = str(Path(__file__).parent) + "/"
 
-# Analyse all FEA computations necessary for non-linear homogenization
+print("Postprocessing non-linear homogenization FEA computations")
 processed_nonlinear_homogenization_results = postprocess_all_homogenization_computations(basedir)
 
-# Plot response and hardening curves
+print("Plotting response and hardening curves")
 plot_all_stress_strain_from_all_results(processed_nonlinear_homogenization_results, basedir + "all_stress_strain.png")
 plot_all_vm_stress_vm_strain_from_all_results(processed_nonlinear_homogenization_results,
                                               basedir + "all_vm_stress_vm_strain.png")
 plot_all_hardening_from_all_results(processed_nonlinear_homogenization_results, basedir + "all_hardening.png")
 
-# Identify yield surface parameters according to predefined criterion, ex: dfa
+
+print("Identifying yield surface parameters")
 dfa_params = identify_plasticity_criterion_parameters(criterion="dfa",
                                                       all_results_dict=processed_nonlinear_homogenization_results)
 np.savetxt(basedir + "dfa_params.txt", dfa_params)
 
-# Plot the identified yield surface in (11, 22) and (11, 12) planes
+print("Plotting yield surface")
 plot_criteria_yield_surface(criterion="dfa",
                             all_results_dict=processed_nonlinear_homogenization_results,
                             criteria_params=dfa_params,
@@ -28,4 +30,3 @@ plot_criteria_shear_yield_surface(criterion="dfa",
                                   all_results_dict=processed_nonlinear_homogenization_results,
                                   criteria_params=dfa_params,
                                   figname=basedir + "dfa_shear_yield_surface.png")
-
