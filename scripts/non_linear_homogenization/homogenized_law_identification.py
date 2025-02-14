fc2069e76c4639091e3295b0f4615de48a183f02
ylgrst <125015797+ylgrst@users.noreply.github.com>
2025-02-15 00:45:20 +0100

    First implementation of identification example script

---
+++ scripts/non_linear_homogenization/homogenized_law_identification.py
@@ -0,0 +1,75 @@
+import numpy as np
+from simcoon import simmit as sim
+from simcoon.parameter import Parameter
+from simcoon.data import Data
+from belem.fem.identification import *
+from belem.utils import ResultsColumnHeader, InputColumnHeader
+
+basedir = str(Path(__file__).parent) + "/identification/"
+os.makedirs(basedir, exist_ok=True)
+
+bulk_material_young = 200.0
+stiffness_tensor = np.loadtxt(str(Path(__file__).parent.parent) + "/linear_homogenization/effective_stiffness_tensor.txt")
+shape_young, _, shape_shear = sim.L_cubic_props(stiffness_tensor)
+poisson_ratio = 0.3
+thermal_expansion_coefficient = 1e-6
+elastic_params = np.array([bulk_material_young*shape_young, poisson_ratio,
+                           bulk_material_young*shape_shear, thermal_expansion_coefficient])
+
+criteria_params = np.loadtxt(str(Path(__file__).parent) + "dfa_params.txt")
+
+sigma_y = Parameter(0, (10.0, 400.0), "@0p", ["material.dat"])
+Q = Parameter(1, (5.0, 500.0), "@1p", ["material.dat"])
+b = Parameter(2, (1.0, 1000.0), "@2p", ["material.dat"])
+C_1 = Parameter(3, (100.0, 30000.0), "@3p", ["material.dat"])
+D_1 = Parameter(4, (5.0, 1000.0), "@4p", ["material.dat"])
+C_2 = Parameter(5, (50.0, 10000.0), "@5p", ["material.dat"])
+D_2 = Parameter(6, (10.0, 1000.0), "@6p", ["material.dat"])
+list_parameters_to_identify = [sigma_y, Q, b, C_1, C_2, D_1, D_2]
+
+data_tension = Data(control=np.loadtxt(str(Path(__file__).parent) + "/tension_cycle/strain.txt") / 100.0,
+                    observation=np.loadtxt(str(Path(__file__).parent) + "/tension_cycle/stress_component.txt"))
+data_biaxial_tension = Data(control=np.loadtxt(str(Path(__file__).parent) + "/biaxial_tension_cycle/strain.txt") / 100.0,
+                            observation=np.loadtxt(str(Path(__file__).parent) + "/biaxial_tension_cycle/stress_component.txt"))
+data_shear = Data(control=np.loadtxt(str(Path(__file__).parent) + "/shear_cycle/strain.txt") / 100.0,
+                  observation=np.loadtxt(str(Path(__file__).parent) + "/shear_cycle/stress_component.txt"))
+list_data = [data_tension, data_biaxial_tension, data_shear]
+
+prepare_epchg_identification(data_to_identify=list_data,
+                             list_columns_to_compare=[[ResultsColumnHeader.S11],
+                                                      [ResultsColumnHeader.S11],
+                                                      [ResultsColumnHeader.S12]],
+                             parameters_to_optimize=list_parameters_to_identify,
+                             elastic_params=elastic_params,
+                             n_iso_hard=1, n_kin_hard=2, criteria="dfa", criteria_params=criteria_params,
+                             basedir=basedir)
+
+os.chdir(basedir)
+
+run_epchg_identification(parameters_to_optimize=list_parameters_to_identify,
+                         elastic_params=elastic_params, n_iso_hard=1, n_kin_hard=2, criteria="dfa",
+                         criteria_params=criteria_params,
+                         path_dir=basedir + "/data/", num_dir=basedir + "/num_data", results_dir=basedir + "/results_id",
+                         popsize=10, tol=0.0001, maxiter=100, disp=True)
+
+plot_graph(sim_list=["tension", "biaxial tension", "shear"],
+           ident_data_columns_to_plot=[[ResultsColumnHeader.E11, ResultsColumnHeader.S11],
+                                       [ResultsColumnHeader.E11, ResultsColumnHeader.S11],
+                                       ResultsColumnHeader.E12, ResultsColumnHeader.S12],
+           exp_data_columns_to_plot=[[InputColumnHeader.STRAIN, InputColumnHeader.STRESS],
+                                     [InputColumnHeader.STRAIN, InputColumnHeader.STRESS],
+                                     [InputColumnHeader.STRAIN, InputColumnHeader.STRESS]],
+           path_results_id=basedir + "/results_id",
+           path_exp=basedir + "/exp_data",
+           )
+
+plot_nrmse(sim_list=["tension", "biaxial tension", "shear"],
+           ident_data_columns_to_plot=[[ResultsColumnHeader.E11, ResultsColumnHeader.S11],
+                                       [ResultsColumnHeader.E11, ResultsColumnHeader.S11],
+                                       ResultsColumnHeader.E12, ResultsColumnHeader.S12],
+           exp_data_columns_to_plot=[[InputColumnHeader.STRAIN, InputColumnHeader.STRESS],
+                                     [InputColumnHeader.STRAIN, InputColumnHeader.STRESS],
+                                     [InputColumnHeader.STRAIN, InputColumnHeader.STRESS]],
+           path_results_id=basedir + "/results_id",
+           path_exp=basedir + "/exp_data",
+           )
